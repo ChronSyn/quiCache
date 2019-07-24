73c4ccbf05e7169a00250074c20aed708ed1ae91
Scott Pritchard <scott.pritchard@korelogic.co.uk>
2019-07-24 10:40:33 +0100

    Exported interfaces in quicache index file

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const quicache_lib_1 = __importDefault(require("./lib/quicache.lib"));
+const quicache_lib_1 = __importStar(require("./lib/quicache.lib"));
+exports.ETimeDuration = quicache_lib_1.ETimeDuration;
 exports.default = quicache_lib_1.default;
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,2 +1,14 @@
-import CacheManager from "./lib/quicache.lib";
+import CacheManager, {
+  ICacheManagerDataCache,
+  ETimeDuration,
+  IConvertStructure,
+  ICacheManager
+} from "./lib/quicache.lib";
+
+export {
+  ICacheManagerDataCache as ICacheManagerDataCache,
+  ETimeDuration as ETimeDuration,
+  IConvertStructure as IConvertStructure,
+  ICacheManager as ICacheManager
+};
 export default CacheManager;
